818ebd247a67c87e03a8b6d3fbcb29ed66eca8ad
John Doe <johndoe@email.com>
2025-06-22 20:26:42 +0530

    SignUp&Login Page

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,14 @@ import authRoutes from "./routes/auth.route.js"; // Importing the auth routes, m
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser"; // Importing cookie-parser to handle cookies and grab values out of it
 import messageRoutes from "./routes/message.route.js"; // Importing message routes, make sure to put .js as type is module
+import cors from "cors"; 
 const app= express();
 app.use(express.json()); // to get values from the body of the request
 app.use(cookieParser()); // to parse cookies from the request headers
+app.use(cors({
+    origin: "http://localhost:5173", // Allow requests from this origin which is the frontend
+    credentials: true, // Allow cookies to be sent with requests
+}));
 dotenv.config()
 const PORT = process.env.PORT || 5001;
 //.use() is a method to mount middleware functions
---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,79 @@
+import {create} from 'zustand';
+import { axiosInstance } from '../lib/axios.js';
+import toast from 'react-hot-toast';
+export const useAuthStore= create((set)=>({
+    authUser: null,
+    isSigningUp: false,
+    isLoggingIn: false,
+    isUpdatingProfile: false,
+    isCheckingAuth:true,
+    checkAuth: async()=>{
+        try{
+            const res = await axiosInstance.get("/auth/check") //localhost:5001 /api already established in instance
+            set({
+                authUser: res.data
+            })
+        }catch(err){
+            console.error("Error checking authentication:", err);
+            set({
+                authUser: null
+            });
+        }finally {
+            set({
+                isCheckingAuth: false
+            });
+        }
+    },
+    signup: async(formdata)=>{
+        set({
+            isSigningUp: true
+        })
+        try{
+            const res = await axiosInstance.post("/auth/signup", formdata);
+            toast.success("Account created successfully!");
+            set({
+                authUser: res.data
+            });
+        
+        }catch(err){
+            toast.error("Error signing up. Please try again"+err.response?.data?.message || "Unknown error");
+            console.error("Error signing up:", err);
+            // Handle error, e.g., show a notification or set an error state
+        }finally {
+            set({
+                isSigningUp: false
+            })
+        }
+    },
+    logout:async()=>{
+        try{
+            await axiosInstance.post("/auth/logout");
+            set({
+                authUser: null
+            });
+            toast.success("Logged out successfully!");
+        }catch(err){
+            console.error("Error logging out:", err);
+            toast.error("Error logging out. Please try again."+ err.response?.data?.message || "Unknown error");
+        }
+    },
+    login:async(formdata)=>{
+        set({
+            isLoggingIn: true
+        })
+        try{
+            const res = await axiosInstance.post("/auth/login", formdata);
+            set({
+                authUser: res.data
+            });
+            toast.success("Logged in successfully!");
+        }catch(error){
+            console.error("Error logging in:", error);
+            toast.error("Error logging in. Please try again."+ error.response?.data?.message || "Unknown error");
+        }finally{
+            set({
+                isLoggingIn: false
+            });
+        }
+    }
+}))
